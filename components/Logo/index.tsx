fb071bd04ee173e0905d1f35814311fb3c233711
Gina Lee <gina@thesistesting.com>
2022-03-09 16:27:59 -0500

    add Logo component

---
+++ components/Logo/index.tsx
@@ -0,0 +1,23 @@
+import classes from './index.module.scss';
+
+const Logo = () => (
+  <div className={classes.wrapper}>
+    <h1>
+      <span className="color_yellow">T</span>
+      <span className="color_red">I</span>
+      <span className="color_yellow">C</span>
+    </h1>
+    <h1>
+      <span className="color_red">T</span>
+      <span className="color_yellow">A</span>
+      <span className="color_red">C</span>
+    </h1>
+    <h1>
+      <span className="color_yellow">T</span>
+      <span className="color_red">O</span>
+      <span className="color_yellow">E</span>
+    </h1>
+  </div>
+)
+
+export default Logo;
