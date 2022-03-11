9b350fd9a50161de11b9615ddbe92bb8dca77574
Gina Lee <gina@thesistesting.com>
2022-03-11 17:44:20 -0500

    render different components based on game status

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,33 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import classes from '../styles/Home.module.scss';
 import Logo from '../components/Logo';
 import SelectGame from '../components/SelectGame';
 import NameCollectionForm from '../components/NameCollectionForm';
+import Game from '../components/Game';
 import useTicTacToe from '../hooks/useTicTacToe';
 
 const Home: NextPage = () => {
-  const { type, handleSelection} = useTicTacToe();
+  const { status, type, players, turn, board, handleSelection, handlePlayerSubmit } = useTicTacToe();
+
+  const start = () => (
+    <>
+      <Logo />
+      {!type ? (
+        <SelectGame handleSelection={handleSelection} />
+      ) : (
+        <NameCollectionForm
+          type={type}
+          status={status}
+          handlePlayerSubmit={handlePlayerSubmit}
+        />
+      )}
+    </>
+  );
 
   return (
-    <div className={styles.container}>
+    <div className={classes.container}>
       <Head>
         <title>Tic Tac Toe</title>
         <meta
@@ -21,13 +37,14 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <Logo />
-
-        {!type ? (
-          <SelectGame handleSelection={handleSelection} />
-        ) : (
-          <NameCollectionForm type={type} />
+      <main className={classes.main}>
+        {status === 'created' && start()}
+        {(status === 'started' || status === 'finished') && (
+          <Game
+            players={players}
+            turn={turn}
+            board={board}
+          />
         )}
       </main>
     </div>
